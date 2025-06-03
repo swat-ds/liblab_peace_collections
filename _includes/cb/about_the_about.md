2ba328e3c88202eb83da3ce4b409032b8b098e53
afayyaz1 <afayyaz1@swarthmore.edu>
2025-06-03 12:59:48 -0400

    Refactor code structure and remove redundant changes

--- _includes/cb/about_the_about.md
+++ _includes/cb/about_the_about.md
@@ -1,14 +1,3 @@
-{% comment %}
-    Find some sample images or use defaults for About demos
-{% endcomment %}
-{% assign imagesample = site.data[site.metadata] | where_exp: 'item','item.format contains "image"' | first %}
-{% capture imagesampleid %}{{ imagesample.objectid | default: "https://www.lib.uidaho.edu/collectionbuilder/demo-objects/mg101_b6_photographs_01.jpg" }}{% endcapture %}
-{% assign pdfsample = site.data[site.metadata] | where_exp: 'item','item.format contains "pdf"' | first %}
-{% capture pdfsampleid %}{{ pdfsample.objectid | default: "https://digital.lib.uidaho.edu/utils/getfile/collection/ui_ep/id/21768/filename/uiext21768.pdf" }}{% endcapture %}
-{% assign videosample = site.data[site.metadata] | where_exp: 'item','item.format contains "video"' | first %}
-{% capture videosampleid %}{{ videosample.objectid | default: "https://cdil.lib.uidaho.edu/storying-extinction/objects/trailcams/videos/ballcreek-cedarrub-birdonpath.mp4" }}{% endcapture %}
-{% assign audiosample = site.data[site.metadata] | where_exp: 'item','item.format contains "audio"' | first %}
-{% capture audiosampleid %}{{ audiosample.objectid | default: "https://www.lib.uidaho.edu/digital/mp3s/Clouds.mp3" }}{% endcapture %}
 
 ## About the About Page
 
---
+++ _site/assets/js/lunr-store.js
@@ -0,0 +1,49 @@
+var store = [{  
+"title": "Ce.1611","creator": "Crabtree, Donald \"Don\" E.","description": "Pressure flaked","subject": "artifacts; lithics", 
+"id": "ce_ca_d1_1611-a.html"
+
+},{  
+"title": "Ce.6500","description": "Burinated distal end, Feels and sounds like plastic. (DHC)","subject": "artifacts; lithics", 
+"id": "ce_ca_d2_6500-a.html"
+
+},{  
+"title": "Ce.7241","creator": "Crabtree, Donald \"Don\" E.","description": "Prismatic, associated w/Ce.7234","subject": "artifacts; lithics", 
+"id": "ce_ca_d2_7241-a.html"
+
+},{  
+"title": "Ce.7940","creator": "Laccetti, M.F.","description": "Part of Donald E. Crabtree's experiments with heat treating stone. Ce.7940 is a fragment of Ce.7939. This specific experiment had the stone heat treated from 8:00am to 3:00pm in electric furnace peaking at 1700 degrees F. The object is a percussion flake and is associated with Ce.7892-Ce.7939.","subject": "experimental artifacts; lithics", 
+"id": "ce_ca_d2_7940_model.html"
+
+},{  
+"title": "Ce.9204","creator": "Crabtree, Donald \"Don\" E.","description": "No contextual data","subject": "experimental artifacts; lithics", 
+"id": "ce_ca_d3_9204-a.html"
+
+},{  
+"title": "Ce.9208","creator": "Crabtree, Donald \"Don\" E.","description": "No contextual data","subject": "experimental artifacts; lithics", 
+"id": "ce_ca_d3_9208-a.html"
+
+},{  
+"title": "Ce.1950","date": "1975-80","creator": "Crabtree, Donald \"Don\" E.","description": "Clovis","subject": "experimental artifacts; lithics", 
+"id": "ce_cc_d2_1950_model.html"
+
+},{  
+"title": "Ce.1951","date": "1972","creator": "Crabtree, Donald \"Don\" E.","description": "Folsom, basally ground","subject": "experimental artifacts; lithics", 
+"id": "ce_cc_d2_1951_model.html"
+
+},{  
+"title": "Ce.1952","creator": "Crabtree, Donald \"Don\" E.","description": "Folsom, distal end, bending fracture","subject": "experimental artifacts; lithics", 
+"id": "ce_cc_d2_1952-a.html"
+
+},{  
+"title": "Ce.1953","creator": "Crabtree, Donald \"Don\" E.","description": "Fluted, ground basal margins","subject": "experimental artifacts; lithics", 
+"id": "ce_cc_d2_1953-a.html"
+
+},{  
+"title": "Ce.1954","creator": "Crabtree, Donald \"Don\" E.","description": "Fluted, Second flute severed the distal end due to too much downward pressure. Fluted by pressure, margins ground to facilitate holding in vise. (JW)Channel flake fragment and distal end renumbered to Ce.10137 and Ce.10138.","subject": "experimental artifacts; lithics", 
+"id": "ce_cc_d2_1954-a.html"
+
+},{  
+"title": "Ce.1152","date": "1950","creator": "Crabtree, Donald \"Don\" E.","description": "Material: glass from bank door. (JW)","subject": "experimental artifacts; lithics", 
+"id": "ce_ce_d1_1152-a.html"
+
+}];
