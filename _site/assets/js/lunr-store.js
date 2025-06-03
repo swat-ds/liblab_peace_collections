368fb9d6d989929b43b533c41641aa08f8738f4c
afayyaz1 <afayyaz1@swarthmore.edu>
2025-06-03 18:32:29 -0400

    Add 3D models and item details for LibLab Bear in the Peace Collection

--- _site/assets/js/lunr-store.js
+++ _site/assets/js/lunr-store.js
@@ -1,5 +1,5 @@
 var store = [{  
-"title": "Ce.1951","date": "1972","creator": "Crabtree, Donald \"Don\" E.","description": "Folsom, basally ground","subject": "experimental artifacts; lithics", 
-"id": "ce_cc_d2_1951_model.html"
+"title": "LibLab Bear","date": "2024","creator": "AF", 
+"id": "1.html"
 
 }];
